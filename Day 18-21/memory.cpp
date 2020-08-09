182caf9fdc86299ff425ec1900559fe08ee72cc9
Cristian <cristianbp11@gmail.com>
2020-08-09 12:00:36 +0200

    Days 18-21 C++

--- Day 18-21/memory.cpp
+++ Day 18-21/memory.cpp
@@ -2,24 +2,31 @@
 #include <math.h>
 #include <string>
 #include <stdlib.h>
-
-
+#include<unistd.h>
 
 using namespace std;
 
 char symbols[8] = {'#','$','%','(',')','/','&','"'};
-void randomChars(char chars[], int len);
+string randomChars(char chars[], int len);
+string userChars(int len);
+bool isCharInSymbols(char ch);
 
 int main()
 {
 
     int TEXT_SIZE = 4;
-    randomChars(symbols, TEXT_SIZE);
+    string randomText = randomChars(symbols, TEXT_SIZE);
+    string userText = userChars(TEXT_SIZE);
+
+    if( randomText == userText )
+        printf("Your sequence is correct! Congratulations!");
+    else
+        printf("Your sequence is wrong! Try it again!");
 
     return 0;
 }
 
-void randomChars(char chars[], int len){
+string randomChars(char chars[], int len){
 
     char textOut[len];
 
@@ -30,6 +37,58 @@ void randomChars(char chars[], int len){
         char randomCh = chars[number];
         textOut[i] = randomCh; 
     }
-    cout << textOut << endl;
 
+    cout << "Try to repeat this sequence: " << endl;
+    cout << textOut;
+
+    sleep(5);
+
+    int j;
+    for(j=0; j<len; j++){
+        printf("\b");
+    }
+
+    return textOut;
+}
+
+string userChars(int len){
+
+    char userText[len];
+
+    int i;
+    for( i=0; i<len; i++){
+        cout << "Type a character include in ";
+        int j;
+        for( j=0; j<8; j++){
+            cout << symbols[j];
+        }
+        cout << endl;
+        cin >> userText[i];
+
+        if( !isCharInSymbols(userText[i]) ){
+            cout << "This character is not valid" << endl;
+            i--;
+        }
+
+    }
+
+    userText[len] = '\0';
+    cout << userText << endl;
+
+    return userText;
+
+}
+
+
+bool isCharInSymbols(char ch){
+
+    int i;
+    for( i=0; i < sizeof(symbols)/sizeof(symbols[0]); i++)
+    {
+        if ( i == sizeof(symbols) - 1 )
+             return false;
+        if( ch == symbols[i] )
+            return true;
+
+    }
 }
